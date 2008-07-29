b84e16cc49cf35aeff13f1b72427aa521543bbfd
Bruce Williams <bruce@codefluency.com>
2008-07-29 18:50:22 -0500

    Add support for contexts Build out `simple' example to show usage * Currently broken against frontend * A few non-context supporting tests broken; on my TODO

--- examples/simple.rb
+++ examples/simple.rb
@@ -10,8 +10,8 @@ require 'fiveruns/dash'
 
 class MyApp
   
-  def self.foos_last_minute
-    rand(80)  
+  def self.foos_last_minute(where)
+    rand(80)
   end
   
   def do_something
@@ -20,16 +20,29 @@ class MyApp
   
 end
 
-Fiveruns::Dash.start :app => 'foo-bar-baz' do |metrics|
-  metrics.counter :foos, "BAR!" do
-    MyApp.foos_last_minute
-  end
-  metrics.time :do_somethings, :method => 'MyApp#do_something'
+class OtherApp < MyApp
 end
+  
+
+Fiveruns::Dash.configure :app => 'foo-bar-baz' do |metrics|
+  metrics.counter :foos, "Number of foos" do
+    {
+      [:place, 'here'] => MyApp.foos_last_minute(:here),
+      [:place, 'there'] => MyApp.foos_last_minute(:there)
+    }    
+  end
+  metrics.time :do_somethings, :method => 'MyApp#do_something', :contexts => lambda { |obj, *args| [:class, obj.class.name] }
+  metrics.counter :somethings, :incremented_by => 'MyApp#do_something', :contexts => lambda { |obj, *args| [:class, obj.class.name] }
+end
+Fiveruns::Dash.configuration.metrics.each_value { |metric| metric.info_id = metric.name }
+Fiveruns::Dash.session.reporter.interval = 10
+Fiveruns::Dash.session.start
 
 app = MyApp.new
+other_app = OtherApp.new
 
 loop do
   sleep rand(3)
   app.do_something
+  other_app.do_something
 end
--- lib/fiveruns/dash/metric.rb
+++ lib/fiveruns/dash/metric.rb
@@ -1,15 +1,7 @@
 require 'dash/typable'
 
 module Fiveruns::Dash
-
-  # Can't override :unit option
-  module LockedUnit
-    def unit_info
-      raise ArgumentError, "Can not set :unit for `#{@name}' #{self.class.metric_type} metric" if @options[:unit]
-      {}
-    end
-  end
-        
+          
   class Metric
     include Typable
     
@@ -20,6 +12,7 @@ module Fiveruns::Dash
       @options = args.extract_options!
       @description = args.shift || @name.titleize
       @operation = block
+      validate!
     end
     
     def info
@@ -38,18 +31,82 @@ module Fiveruns::Dash
     private
     #######
     
+    def validate!
+      true
+    end
+    
     def unit_info
        @options[:unit] ? {:unit => @options[:unit].to_s} : {}
     end
     
     def value_hash
-      {:value => @operation.call}
+      {:value => parse_value(@operation.call)}
+    end
+    
+    # Verifies value matches one of the following patterns:
+    # * A numeric value (indicates no namespace)
+    # * A hash of [namespace_kind, namespace_name, ...] => value pairs, eg:
+    #     [:controller, 'FooController', :action, 'bar'] => 12
+    def parse_value(value)
+      case value
+      when Numeric
+        value
+      when Hash
+        value.inject({}) do |all, (key, val)|
+          case key
+          when nil
+            all[key] = val
+          when Array
+            if key.size % 2 == 0
+              all[key.in_groups_of(2)] = val
+            else
+              bad_value! "Contexts must have an even number of items"
+            end
+          else
+            bad_value! "Unknown context type"
+          end
+          all
+        end
+      else
+        bad_value! "Unknown value type"
+      end
+    end
+    
+    def bad_value!(message)
+      raise ArgumentError, "Bad data for `#{@name}' #{self.class.metric_type} metric: #{message}"
+    end
+    
+    # Note: only to be used when the +@operation+
+    #       block is used to set contexts
+    def find_containers(*args, &block) #:nodoc:
+      contexts = context_finder.call(*args)
+      if contexts.all? { |item| !item.is_a?(Array) }
+        contexts = [contexts]
+      end
+      if contexts.empty? || contexts == [[]]
+        contexts = [nil]
+      end
+      contexts.each do |context|
+        with_container_for_context(context, &block)
+      end
+    end
+    
+    def with_container_for_context(context)
+      container = @data[context]
+      new_container = yield container
+      @data[context] = new_container # For hash defaults
+    end
+    
+    def context_finder
+      @context_finder ||= begin
+        context_setting = @options[:context] || @options[:contexts]
+        context_setting.is_a?(Proc) ? context_setting : lambda { |*args| Array(context_setting) } 
+      end
     end
     
   end
   
   class TimeMetric < Metric
-    include LockedUnit
     
     def initialize(*args)
       super(*args)
@@ -62,29 +119,37 @@ module Fiveruns::Dash
     #######
     
     def value_hash
-      returning(:value => @time, :invocations => @invocations) do
+      returning(:value => @data) do
         reset
       end
     end
     
     def reset
-      @invocations = @time = 0
+      @data = Hash.new {{ :invocations => 0, :time => 0 }}
     end
 
     def install_hook
-      if methods_to_instrument.blank?
-        raise ArgumentError, "Must set :method or :methods option for `#{@name}` time metric"
-      end
+      @operation ||= lambda { nil }
       methods_to_instrument.each do |meth|
         Instrument.add meth do |obj, time, *args|
-          @invocations += 1
-          @time += time
+          find_containers(obj, *args) do |container|
+            container[:invocations] += 1
+            container[:time] += time
+            container
+          end
         end
       end
     end
     
     def methods_to_instrument
       @methods_to_instrument ||= Array(@options[:method]) + Array(@options[:methods])
+    end
+    
+    def validate!
+      raise ArgumentError, "Can not set :unit for `#{@name}' time metric" if @options[:unit]
+      if methods_to_instrument.blank?
+        raise ArgumentError, "Must set :method or :methods option for `#{@name}` time metric"
+      end
     end
     
   end
@@ -101,7 +166,7 @@ module Fiveruns::Dash
     
     def value_hash
       if incrementing_methods.any?
-        returning(:value => @invocations) do
+        returning(:value => @data) do
           reset
         end
       else
@@ -113,25 +178,39 @@ module Fiveruns::Dash
       if incrementing_methods.blank?
         raise RuntimeError, "Bad configuration for `#{@name}` counter metric"
       end
+      @operation ||= lambda { nil }
       incrementing_methods.each do |meth|
-        Instrument.add meth do |*args|
-          @invocations += 1
+        Instrument.add meth do |obj, time, *args|
+          find_containers(obj, *args) do |container|
+            container += 1
+            container
+          end
         end
       end
     end
     
     def reset
-      @invocations = 0
+      @data = Hash.new(0)
     end     
     
     def incrementing_methods
       @incrementing_methods ||= Array(@options[:incremented_by])
     end
     
+    def validate!
+      if !@options[:incremented_by]
+        raise ArgumentError, "No block given to capture counter `#{@name}'" unless @operation
+      end
+    end
+    
   end
   
   class PercentageMetric < Metric
-    include LockedUnit
+    
+    def validate!
+      raise ArgumentError, "Can not set :unit for `#{@name}' percentage metric" if @options[:unit]
+    end
+      
   end
   
   class AbsoluteMetric < Metric
