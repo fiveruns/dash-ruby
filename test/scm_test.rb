1576f7f7e735ae6c4ff552b843c65c39d68475ac
Mike Perham <mperham@gmail.com>
2008-08-21 10:48:04 -0500

    SCM test assumes git directory layout, not true in CI environment. Lowest common denominator, here we come!

--- test/scm_test.rb
+++ test/scm_test.rb
@@ -30,7 +30,7 @@ class ScmTest < Test::Unit::TestCase
   
   context "All SCMs" do
     should "find .git in gems/fiveruns_dash on locate_upwards" do
-      assert_match %r(gems/fiveruns_dash$), SCM.send(:locate_upwards, File.dirname(__FILE__), ".git" )
+      assert_match %r(fiveruns_dash), SCM.send(:locate_upwards, File.dirname(__FILE__), ".git" )
     end
 
     should "return nil from locate_upwards when no .git" do
