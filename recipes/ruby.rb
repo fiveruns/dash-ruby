dd6958a07fcc63fd22f24bd1344b64eb5a5e4976
Bruce Williams <bruce@codefluency.com>
2008-08-25 14:14:01 -0500

    Add `pmem' metric

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -2,7 +2,10 @@ Fiveruns::Dash.register_recipe :ruby, :url => 'http://dash.fiveruns.com' do |met
   metrics.absolute :rss, "Resident Memory", :unit => 'byte' do 
     Integer(`ps -o rss -p #{Process.pid}`[/(\d+)/, 1])
   end
-  metrics.percentage :cpu_perc, 'CPU Usage' do
+  metrics.absolute :pmem, "Resident Memory" do 
+    Float(`ps -o pmem -p #{Process.pid}`[/(\d+\.\d+)/, 1])
+  end  
+  metrics.percentage :cpu, 'CPU Usage' do
     (Process.times.utime / ::Fiveruns::Dash.process_age) * 100.00
   end
 end
