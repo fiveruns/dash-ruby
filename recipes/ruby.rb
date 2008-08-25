f34dcf6f6a91743ffbe85c63dd900a139aad5a45
Bruce Williams <bruce@codefluency.com>
2008-08-25 16:20:49 -0500

    `pmem' metric is a percentage, not a absolute

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -2,7 +2,7 @@ Fiveruns::Dash.register_recipe :ruby, :url => 'http://dash.fiveruns.com' do |met
   metrics.absolute :rss, "Resident Memory", :unit => 'byte' do 
     Integer(`ps -o rss -p #{Process.pid}`[/(\d+)/, 1])
   end
-  metrics.absolute :pmem, "Resident Memory" do 
+  metrics.percentage :pmem, "Resident Memory" do 
     Float(`ps -o pmem -p #{Process.pid}`[/(\d+\.\d+)/, 1])
   end  
   metrics.percentage :cpu, 'CPU Usage' do
