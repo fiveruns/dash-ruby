4300269423a8cfb03cb65f9f4bae6ca0895c9774
Eric Falcao <efalcao@gmail.com>
2008-10-21 10:38:45 -0500

    removed host and app aggregation

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,11 +1,11 @@
 Fiveruns::Dash.register_recipe :ruby, :url => 'http://dash.fiveruns.com' do |metrics|
-  metrics.absolute :rss, "Resident Memory Usage", "The Absolute Resident Memory Usage", :unit => 'bytes', :aggregate => {:host => :sum, :app => :average} do 
+  metrics.absolute :rss, "Resident Memory Usage", "The Absolute Resident Memory Usage", :unit => 'bytes' do 
     Integer(`ps -o rss -p #{Process.pid}`[/(\d+)/, 1])
   end
-  metrics.percentage :pmem, "Resident Memory Usage", "The Percentage Resident Memopry Usage", :aggregate => {:host => :sum, :app => :average} do 
+  metrics.percentage :pmem, "Resident Memory Usage", "The Percentage Resident Memopry Usage" do 
     Float(`ps -o pmem -p #{Process.pid}`[/(\d+\.\d+)/, 1])
   end  
-  metrics.percentage :cpu, 'CPU Usage', 'Percentage CPU Usage', :aggregate => {:host => :sum, :app => :average} do
+  metrics.percentage :cpu, 'CPU Usage', 'Percentage CPU Usage' do
     (Process.times.utime / ::Fiveruns::Dash.process_age) * 100.00
   end
 end
